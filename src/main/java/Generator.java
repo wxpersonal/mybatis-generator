443257eef2ed66d04d88b683affb712215e462a2
wxpersonal <wxpersonal@163.com>
2017-06-16 13:50:49 +0800

    一些修改

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,3 +1,7 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.File;
 import java.net.URL;
