98c3f047517edff9782c5aea4f95ed601c622142
weixiang <weixiang@xiaomi.com>
2021-10-28 18:37:30 +0800

    batchInsert

--- src/main/java/me/weix/whatever/plugins/CustomAbstractXmlElementGenerator.java
+++ src/main/java/me/weix/whatever/plugins/CustomAbstractXmlElementGenerator.java
@@ -2,7 +2,7 @@ package me.weix.whatever.plugins;
 
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.dom.OutputUtilities;
-import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
@@ -12,81 +12,21 @@ import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElem
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class CustomAbstractXmlElementGenerator extends AbstractXmlElementGenerator {
 
 	@Override
 	public void addElements(XmlElement parentElement) {
-		TextElement selectText = getBaseXml(parentElement);
 		// 公用include
 		XmlElement include = new XmlElement("include");
 		include.addAttribute(new Attribute("refid", "base_query"));
 
-//		addXmlFind(parentElement, selectText, include);
-		addXmlSelectByCondition(parentElement, selectText, include);
+		addXmlSelectByCondition(parentElement);
 		addXmlSelectByIds(parentElement);
-//		addXmlLogicDeleteById(parentElement);
-//		addXmlLogicDeleteByIds(parentElement);
 		addXmlDeleteByIds(parentElement);
 		addXmlBatchInsert(parentElement);
-	}
-
-
-	private TextElement getBaseXml(XmlElement parentElement) {
-		// 增加base_query
-		XmlElement sql = new XmlElement("sql");
-		sql.addAttribute(new Attribute("id", "base_query"));
-		//在这里添加where条件
-		XmlElement selectTrimElement = new XmlElement("trim"); //设置trim标签
-		selectTrimElement.addAttribute(new Attribute("prefix", "WHERE"));
-		selectTrimElement.addAttribute(new Attribute("prefixOverrides", "AND | OR")); //添加where和and
-		StringBuilder sb = new StringBuilder();
-		for(IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
-			XmlElement selectNotNullElement = new XmlElement("if"); //$NON-NLS-1$
-			sb.setLength(0);
-			sb.append("null != ");
-			sb.append(introspectedColumn.getJavaProperty());
-			selectNotNullElement.addAttribute(new Attribute("test", sb.toString()));
-			sb.setLength(0);
-			// 添加and
-			sb.append(" and ");
-			// 添加别名t
-			sb.append("t.");
-			sb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
-			// 添加等号
-			sb.append(" = ");
-			sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn));
-            selectNotNullElement.addElement(new TextElement(sb.toString()));
-            selectTrimElement.addElement(selectNotNullElement);
-		}
-		sql.addElement(selectTrimElement);
-		parentElement.addElement(sql);
-
-		// 公用select
-		sb.setLength(0);
-		sb.append("select ");
-		sb.append("t.* ");
-		sb.append("from ");
-		sb.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
-		sb.append(" t");
-		TextElement selectText = new TextElement(sb.toString());
-		return selectText;
-	}
-
-	private void addXmlLogicDeleteByIds(XmlElement parentElement) {
-		XmlElement deleteLogicByIdsElement = new XmlElement("update");
-		deleteLogicByIdsElement.addAttribute(new Attribute("id", "deleteLogicByIds"));
-		deleteLogicByIdsElement.addAttribute(new Attribute("parameterType", "java.util.List"));
-
-		deleteLogicByIdsElement.addElement(
-				new TextElement(
-						"update " + introspectedTable.getFullyQualifiedTableNameAtRuntime() + " set deleted = 1 where id in \n"
-								+ "\t<foreach item=\"item\" index=\"index\" collection=\"ids\" open=\"(\" separator=\",\" close=\")\">#{item}</foreach> "
-				));
-
-		parentElement.addElement(deleteLogicByIdsElement);
+//		addXmlLogicDeleteById(parentElement);
+//		addXmlLogicDeleteByIds(parentElement);
 	}
 
 	private void addXmlDeleteByIds(XmlElement parentElement) {
@@ -119,31 +59,7 @@ public class CustomAbstractXmlElementGenerator extends AbstractXmlElementGenerat
 		parentElement.addElement(selectByIdsElement);
 	}
 
-	private void addXmlLogicDeleteById(XmlElement parentElement) {
-		XmlElement deleteLogicByIdElement = new XmlElement("update");
-		deleteLogicByIdElement.addAttribute(new Attribute("id", "deleteLogicById"));
-		deleteLogicByIdElement.addAttribute(new Attribute("parameterType", "java.lang.Integer"));
-
-		deleteLogicByIdElement.addElement(
-				new TextElement(
-						"update " + introspectedTable.getFullyQualifiedTableNameAtRuntime() + " set deleted = 1 where id = #{id} "
-				));
-
-		parentElement.addElement(deleteLogicByIdElement);
-	}
-
-	private void addXmlFind(XmlElement parentElement, TextElement selectText, XmlElement include) {
-		// 增加find
-		XmlElement find = new XmlElement("select");
-		find.addAttribute(new Attribute("id", "find"));
-		find.addAttribute(new Attribute("resultMap", "BaseResultMap"));
-		find.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
-		find.addElement(selectText);
-		find.addElement(include);
-		parentElement.addElement(find);
-	}
-
-	private void addXmlSelectByCondition(XmlElement parentElement, TextElement selectText, XmlElement include) {
+	private void addXmlSelectByCondition(XmlElement parentElement) {
 		// 增加find
 		XmlElement find = new XmlElement("select");
 		find.addAttribute(new Attribute("id", "selectByCondition"));
@@ -151,8 +67,41 @@ public class CustomAbstractXmlElementGenerator extends AbstractXmlElementGenerat
 		find.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
 		new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
 
-		find.addElement(selectText);
-		find.addElement(include);
+		XmlElement selectTrimElement = new XmlElement("trim"); //设置trim标签
+		selectTrimElement.addAttribute(new Attribute("prefix", "WHERE"));
+		selectTrimElement.addAttribute(new Attribute("prefixOverrides", "AND | OR")); //添加where和and
+		StringBuilder sb = new StringBuilder();
+		for(IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
+			XmlElement selectNotNullElement = new XmlElement("if"); //$NON-NLS-1$
+			sb.setLength(0);
+			sb.append("null != ");
+			sb.append(introspectedColumn.getJavaProperty());
+			selectNotNullElement.addAttribute(new Attribute("test", sb.toString()));
+			sb.setLength(0);
+			// 添加and
+			sb.append(" and ");
+			// 添加别名t
+			sb.append("t.");
+			sb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+			// 添加等号
+			sb.append(" = ");
+			sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn));
+			selectNotNullElement.addElement(new TextElement(sb.toString()));
+			selectTrimElement.addElement(selectNotNullElement);
+		}
+
+		// 公用select
+		sb.setLength(0);
+		sb.append("select ");
+		find.addElement(new TextElement(sb.toString()));
+		find.addElement(getBaseColumnListElement());
+
+		sb.setLength(0);
+		sb.append("from ");
+		sb.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
+		sb.append(" t");
+		find.addElement(new TextElement(sb.toString()));
+		find.addElement(selectTrimElement);
 		parentElement.addElement(find);
 	}
 
@@ -213,4 +162,30 @@ public class CustomAbstractXmlElementGenerator extends AbstractXmlElementGenerat
 
 	}
 
+	private void addXmlLogicDeleteById(XmlElement parentElement) {
+		XmlElement deleteLogicByIdElement = new XmlElement("update");
+		deleteLogicByIdElement.addAttribute(new Attribute("id", "deleteLogicById"));
+		deleteLogicByIdElement.addAttribute(new Attribute("parameterType", "java.lang.Integer"));
+
+		deleteLogicByIdElement.addElement(
+				new TextElement(
+						"update " + introspectedTable.getFullyQualifiedTableNameAtRuntime() + " set deleted = 1 where id = #{id} "
+				));
+
+		parentElement.addElement(deleteLogicByIdElement);
+	}
+
+	private void addXmlLogicDeleteByIds(XmlElement parentElement) {
+		XmlElement deleteLogicByIdsElement = new XmlElement("update");
+		deleteLogicByIdsElement.addAttribute(new Attribute("id", "deleteLogicByIds"));
+		deleteLogicByIdsElement.addAttribute(new Attribute("parameterType", "java.util.List"));
+
+		deleteLogicByIdsElement.addElement(
+				new TextElement(
+						"update " + introspectedTable.getFullyQualifiedTableNameAtRuntime() + " set deleted = 1 where id in \n"
+								+ "\t<foreach item=\"item\" index=\"index\" collection=\"ids\" open=\"(\" separator=\",\" close=\")\">#{item}</foreach> "
+				));
+
+		parentElement.addElement(deleteLogicByIdsElement);
+	}
 }
--- src/main/java/me/weix/whatever/plugins/CustomJavaMapperMethodGenerator.java
+++ src/main/java/me/weix/whatever/plugins/CustomJavaMapperMethodGenerator.java
@@ -1,6 +1,5 @@
 package me.weix.whatever.plugins;
 
-import org.apache.ibatis.annotations.Param;
 import org.mybatis.generator.api.dom.java.*;
 import org.mybatis.generator.codegen.mybatis3.javamapper.elements.AbstractJavaMapperMethodGenerator;
 
@@ -13,14 +12,13 @@ public class CustomJavaMapperMethodGenerator extends AbstractJavaMapperMethodGen
 	public void addInterfaceElements(Interface interfaze) {
 		interfaze.addImportedType(new FullyQualifiedJavaType("org.apache.ibatis.annotations.Param"));
 		interfaze.addImportedType(new FullyQualifiedJavaType("java.util.List"));
-//		addInterfaceFind(interfaze);
-//		addInterfaceDeleteLogicById(interfaze);
-//		addInterfaceDeleteLogicByIds(interfaze);
+
 		addInterfaceSelectByIds(interfaze);
 		addInterfaceSelectByCondition(interfaze);
-//		addInterfacePageList(interfaze);
 		addInterfaceDeleteByIds(interfaze);
 		addInterfaceBatchInsert(interfaze);
+//		addInterfaceDeleteLogicById(interfaze);
+//		addInterfaceDeleteLogicByIds(interfaze);
 	}
 
 	private void addInterfaceBatchInsert(Interface interfaze) {
@@ -46,58 +44,6 @@ public class CustomJavaMapperMethodGenerator extends AbstractJavaMapperMethodGen
 			interfaze.addMethod(method);
 		}
 	}
-	
-	private void addInterfaceFind(Interface interfaze) {
-		// 先创建import对象
-		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<>();
-		// 添加List的包
-		importedTypes.add(FullyQualifiedJavaType.getNewListInstance());
-		// 创建方法对象
-		Method method = new Method("find");
-		// 设置该方法为public
-		method.setVisibility(JavaVisibility.PUBLIC);
-		// 设置返回类型是List
-		FullyQualifiedJavaType returnType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		// 方法对象设置返回类型对象
-		method.setReturnType(returnType);
-		// 设置方法名称为我们在IntrospectedTable类中初始化的 “selectByObject”
-
-		// 设置参数类型是对象
-		FullyQualifiedJavaType parameterType;
-		parameterType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		// import参数类型对象
-		importedTypes.add(parameterType);
-		// 为方法添加参数，变量名称record
-		method.addParameter(new Parameter(parameterType, "record")); //$NON-NLS-1$
-		//
-		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
-		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
-			interfaze.addImportedTypes(importedTypes);
-			interfaze.addMethod(method);
-		}
-	}
-
-	/*
-	 * Dao中添加方法
-	 */
-	private void addInterfaceDeleteLogicByIds(Interface interfaze) {
-		// 先创建import对象
-		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
-		Method method = new Method("deleteLogicByIds");
-		method.setVisibility(JavaVisibility.PUBLIC);
-		method.setReturnType(FullyQualifiedJavaType.getIntInstance());
-		method.addParameter(new Parameter(new FullyQualifiedJavaType("Integer[]"), "ids", "@Param(\"ids\")"));
-		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
-		// 设置参数类型是对象
-		FullyQualifiedJavaType parameterType;
-		parameterType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		// import参数类型对象
-		importedTypes.add(parameterType);
-		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
-			interfaze.addImportedTypes(importedTypes);
-			interfaze.addMethod(method);
-		}
-	}
 
 	/*
 	 * Dao中添加方法
@@ -117,19 +63,6 @@ public class CustomJavaMapperMethodGenerator extends AbstractJavaMapperMethodGen
 		importedTypes.add(parameterType);
 		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
 			interfaze.addImportedTypes(importedTypes);
-			interfaze.addMethod(method);
-		}
-	}
-
-	private void addInterfaceDeleteLogicById(Interface interfaze) {
-
-		Method method = new Method("deleteLogicById");
-		method.setVisibility(JavaVisibility.PUBLIC);
-		method.setReturnType(FullyQualifiedJavaType.getIntInstance());
-		method.addParameter(new Parameter(new FullyQualifiedJavaType("Integer"), "id"));
-		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
-		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
-		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
 			interfaze.addMethod(method);
 		}
 	}
@@ -184,42 +117,39 @@ public class CustomJavaMapperMethodGenerator extends AbstractJavaMapperMethodGen
 			interfaze.addMethod(method);
 		}
 	}
-//
-	private void addInterfacePageList(Interface interfaze) {
-		// 先创建import对象
-		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<>();
-		// 添加Page的包
-		importedTypes.add(new FullyQualifiedJavaType("com.github.pagehelper.Page"));
-		// 创建方法对象
-		Method method = new Method();
-		// 设置该方法为public
-		method.setVisibility(JavaVisibility.PUBLIC);
-		// 设置返回类型是List
-		FullyQualifiedJavaType returnType = new FullyQualifiedJavaType("Page");
-		FullyQualifiedJavaType listType;
-		// 设置List的类型是实体类的对象
-		listType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		importedTypes.add(listType);
-		// 返回类型对象设置为List
-		returnType.addTypeArgument(listType);
-		// 方法对象设置返回类型对象
-		method.setReturnType(returnType);
-		// 设置方法名称为我们在IntrospectedTable类中初始化的 “selectByObject”
-		method.setName("pageList");
 
+	private void addInterfaceDeleteLogicById(Interface interfaze) {
+
+		Method method = new Method("deleteLogicById");
+		method.setVisibility(JavaVisibility.PUBLIC);
+		method.setReturnType(FullyQualifiedJavaType.getIntInstance());
+		method.addParameter(new Parameter(new FullyQualifiedJavaType("Integer"), "id"));
+		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
+		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
+		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
+			interfaze.addMethod(method);
+		}
+	}
+
+	/*
+	 * Dao中添加方法
+	 */
+	private void addInterfaceDeleteLogicByIds(Interface interfaze) {
+		// 先创建import对象
+		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
+		Method method = new Method("deleteLogicByIds");
+		method.setVisibility(JavaVisibility.PUBLIC);
+		method.setReturnType(FullyQualifiedJavaType.getIntInstance());
+		method.addParameter(new Parameter(new FullyQualifiedJavaType("Integer[]"), "ids", "@Param(\"ids\")"));
+		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
 		// 设置参数类型是对象
 		FullyQualifiedJavaType parameterType;
 		parameterType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
 		// import参数类型对象
 		importedTypes.add(parameterType);
-		// 为方法添加参数，变量名称record
-		method.addParameter(new Parameter(parameterType, "record")); //$NON-NLS-1$
-		//
-		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
 		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
 			interfaze.addImportedTypes(importedTypes);
 			interfaze.addMethod(method);
 		}
 	}
-
 }
